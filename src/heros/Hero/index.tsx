53d0b440683d9b3f097d91cb2382acfc1bcdebb8
Emmanuel Valenzuela <manuvaglz@gmail.com>
2026-07-11 20:31:57 -0600

    Use next/image for the Hero background
    Replace the CSS background-image (which can't host a React component)
    with a fill-positioned next/image, layering the gradient overlay and
    text as separate absolutely-positioned siblings. Gets automatic image
    optimization for the hero photo.

--- src/heros/Hero/index.tsx
+++ src/heros/Hero/index.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 type HeroProps = {
   image: string
   children: React.ReactNode
@@ -5,13 +7,17 @@ type HeroProps = {
 
 const Hero = ({ image, children }: HeroProps) => {
   return (
-    <div
-      className="bg-cover bg-center h-screen max-h-[746px] shrink-0 flex flex-col"
-      style={{
-        backgroundImage: `linear-gradient(to bottom, #000000 0%, #66666682 50%), url(${image})`,
-      }}
-    >
-      <div className="container flex flex-col items-end justify-center  flex-1 text-white">
+    <div className="relative h-screen max-h-[746px] shrink-0 flex flex-col">
+      <Image src={image} alt="" fill priority className="object-cover object-center" />
+
+      <div
+        className="absolute inset-0"
+        style={{
+          backgroundImage: 'linear-gradient(to bottom, #000000 0%, #66666682 50%)',
+        }}
+      />
+
+      <div className="container relative flex flex-1 flex-col items-end justify-center text-white">
         {children}
         <hr className="h-0.5 w-24 bg-accent border-0 mt-8" />
       </div>
